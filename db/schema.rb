a346205c5e556744ec7c21e1ee4212ce08a2303b
iwasatat0107 <it.was07@gmail.com>
2021-02-25 06:10:05 +0900

    tweetsテーブルから不要なカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_235051) do
+ActiveRecord::Schema.define(version: 2021_02_24_210733) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
